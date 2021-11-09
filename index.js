b38eef1acaea18078ff7902a3b2a0c5441088ef5
VazquezDev37 <isaacvazquez1119@gmail.com>
2021-11-08 20:28:08 -0600

    final updates for backend

--- index.js
+++ index.js
@@ -1,24 +1,18 @@
-const connectDB = require('./startup/db'); 
-const express = require('express'); 
+const { connectDB } = require("./startup/db");
+const express = require("express");
 const app = express();
-const user = require('./routes/users');
-const cors = require('cors');
+const user = require("./routes/users");
+const cors = require("cors");
 
-
-
- 
 connectDB();
+
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use('/api/users', user);
+app.use(express.urlencoded({ extended: true }));
+app.use("/api/users", user);
 
-
-
-
- 
-const port = process.env.PORT || 5000; 
-app.listen(port, () => { 
-  console.log(`Server started on port: ${port}`); 
-}); 
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server started on port: ${port}`);
+});
--- startup/db.js
+++ startup/db.js
@@ -2,16 +2,36 @@ const mongoose = require('mongoose');
 const config = require('config');
  
  
-function connectDB() { 
+
+const { MongoClient } = require("mongodb");
+const client = new MongoClient(config.get("mongoURI"), {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+
+async function makeClient() {
+  let conn;
+  try {
+    conn = await client.connect();
+  } catch (err) {
+    console.log(`Could not connect to MongoDB. ERROR: ${err}`);
+    process.exit(1);
+  }
+  return conn;
+}
+
+function connectDB() {
   mongoose
-    .connect( 
-      config.get('mongoURI'),  
-      { useNewUrlParser: true, useUnifiedTopology: true }) 
-    .then(() => console.log('Connected to MongoDB...')) 
-    .catch((err) => { 
-      console.log(`Could not connect to MongoDB. ERROR: ${err}`); 
-      process.exit(1); 
-    }); 
-} 
- 
-module.exports = connectDB;
+    .connect(config.get("mongoURI"), {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log("Connected to MongoDB..."))
+    .catch((err) => {
+      console.log(`Could not connect to MongoDB. ERROR: ${err}`);
+      process.exit(1);
+    });
+}
+
+module.exports = { connectDB, makeClient };
